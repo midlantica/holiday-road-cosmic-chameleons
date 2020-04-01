2bbb43a34c4242a02bae9f6da34fded89c51e560
Andrew Harper <druharper@gmail.com>
2020-04-01 09:55:32 -0500

    eatery dialog good

--- scripts/eateries/EateryDialog.js
+++ scripts/eateries/EateryDialog.js
@@ -1,36 +1,33 @@
-import { useEateries } from "./EateryDataProvider.js"
+import { useEateries } from './EateryDataProvider.js'
 
-const contentTarget = document.querySelector(".eateryDialog")
-const eventHub = document.querySelector(".container")
+const contentTarget = document.querySelector('.dialog__eatery')
+const eventHub = document.querySelector('.container')
 
-eventHub.addEventListener("eateryDetailBtnClicked", customEvent => {
-    // Get the eatery id
-    const eateryId = customEvent.detail.clickedEatery
+eventHub.addEventListener('eateryDetailBtnClicked', customEvent => {
+  // Get the eatery id
+  const eateryId = customEvent.detail.clickedEatery
 
-    const eateryArray = useEateries()
+  const eateryArray = useEateries()
 
-    // Find returns the very first object that matches the condition in the callback function
-    const iFoundYou = eateryArray.find(
-        (currentEatery) => {
-            if (currentEatery.id === parseInt(eateryId)) {
-                return true
-            }
-            return false
-        }
-    )
+  // Find returns the very first object that matches the condition in the callback function
+  const iFoundYou = eateryArray.find(currentEatery => {
+    if (currentEatery.id === parseInt(eateryId)) {
+      return true
+    }
+    return false
+  })
 
-    EateryDialog(iFoundYou)
-    contentTarget.showModal()
+  EateryDialog(iFoundYou)
+  contentTarget.showModal()
 })
 
-export const EateryDialog = (eateryObject) => {
-
-    contentTarget.innerHTML = `        
+export const EateryDialog = eateryObject => {
+  contentTarget.innerHTML = `
         <section class="eatery">
             <h4>${eateryObject.businessName}</h>
             <p>${eateryObject.description}</p>
             <p>${eateryObject.city}</p>
             <p>${eateryObject.state}</p>
-        </section>  
+        </section>
     `
 }

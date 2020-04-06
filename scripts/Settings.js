97aba67c4191b4154937a9f26ca7eea8e2d06459
Andrew Harper <druharper@gmail.com>
2020-04-06 11:30:22 -0500

    Settings APIs to show for preview

---
+++ scripts/Settings.js
@@ -0,0 +1,5 @@
+export const keys = {
+  graphhopperKey: '',
+  npsKey: 'XjgXFaWEevp2aJdzjA4oGmz2GSBJUbhcHlBo9Juo',
+  weatherKey: 'aac7b45e791504a0d5791694a2fa43c8',
+}
--- scripts/weather/WeatherDataProvider.js
+++ scripts/weather/WeatherDataProvider.js
@@ -1,6 +1,6 @@
 import { keys } from '../Settings.js'
 
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector('.container')
 
 // Stores the returned value of the weather API into an empty object (because the data returned back (of parsedweather) is an object):
 
@@ -9,14 +9,16 @@ let weather = {}
 // Dispatches custom event notifying other modules that a weather state change has occurred:
 
 const dispatchWeatherChangeEvent = () => {
-  const weatherStateChangedEvent = new CustomEvent("weatherStateChanged")
+  const weatherStateChangedEvent = new CustomEvent('weatherStateChanged')
   eventHub.dispatchEvent(weatherStateChangedEvent)
 }
 
-// Fetches 5-day weather forecast by zip code, stores it in "weather" object, and dispatches: 
+// Fetches 5-day weather forecast by zip code, stores it in "weather" object, and dispatches:
 
 export const getWeather = (postalCode) => {
-  return fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${postalCode}&units=imperial&&appid=${keys.weatherKey}`)
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?zip=${postalCode}&units=imperial&&appid=${keys.weatherKey}`
+  )
     .then((response) => response.json())
     .then((parsedWeather) => {
       weather = parsedWeather.list[0]
@@ -24,7 +26,7 @@ export const getWeather = (postalCode) => {
     .then(dispatchWeatherChangeEvent)
 }
 
-// Exports a copy of the updated weather object for use by other modules: 
+// Exports a copy of the updated weather object for use by other modules:
 
 export const useWeather = () => {
   return weather
